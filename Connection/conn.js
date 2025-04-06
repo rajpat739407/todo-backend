8183a465bae0c6f032ecdf57587a3087695790e7
Raj Patel <rajp739407@gmail.com>
2025-04-07 02:49:21 +0530

    Update mongoDB warning

--- Connection/conn.js
+++ Connection/conn.js
@@ -3,10 +3,7 @@ require("dotenv").config(); // make sure this is here
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected successfully!");
   } catch (error) {
     console.error("Error in MongoDB connection:", error.message);
