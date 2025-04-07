07d395155edb753d2d9338bf1b7b2517b10baa61
Raj Patel <rajp739407@gmail.com>
2025-04-07 14:09:32 +0530

    ✅ Fixed CORS setup for Vercel

--- Connection/conn.js
+++ Connection/conn.js
@@ -4,7 +4,7 @@ require("dotenv").config(); // make sure this is here
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.lozg("MongoDB connected successfully!");
+    console.log("MongoDB connected successfully!");
   } catch (error) {
     console.error("Error in MongoDB connection:", error.message);
     process.exit(1);
